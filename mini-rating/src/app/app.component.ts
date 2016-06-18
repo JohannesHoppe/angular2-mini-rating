3053f5a88d8ee365a0d53420023aef8870a1adfe
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-06-19 01:38:53 +0200

    simple material app

--- mini-rating/src/app/app.component.ts
+++ mini-rating/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MdList } from '@angular2-material/list/';
+import { MdToolbar } from '@angular2-material/toolbar';
+import { MdButton } from '@angular2-material/button';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [MdList, MdToolbar, MdButton]
 })
 export class AppComponent {
   title = 'app works!';
