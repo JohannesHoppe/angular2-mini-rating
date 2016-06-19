2ba620d614e45727d9eddd1f149d3a2c61eec05c
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-06-19 02:23:32 +0200

    heureka!

--- mini-rating/src/app/app.component.ts
+++ mini-rating/src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { MdList } from '@angular2-material/list/';
 import { MdToolbar } from '@angular2-material/toolbar';
 import { MdButton } from '@angular2-material/button';
+import { MdSlideToggle } from '@angular2-material/slide-toggle';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [MdList, MdToolbar, MdButton]
+  directives: [MdList, MdToolbar, MdButton, MdSlideToggle]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'DWX app works!';
 }
