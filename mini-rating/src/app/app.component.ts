05e448569a76e79d52d24f25392c28417c37ab09
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-06-20 12:25:37 +0200

    md-list-item with book-monkey

--- mini-rating/src/app/app.component.ts
+++ mini-rating/src/app/app.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
-import { MdList } from '@angular2-material/list/';
-import { MdToolbar } from '@angular2-material/toolbar';
-import { MdButton } from '@angular2-material/button';
-import { MdSlideToggle } from '@angular2-material/slide-toggle';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'; // not forget: HTTP_PROVIDERS
+import { MD_LIST_DIRECTIVES } from '@angular2-material/list/';
+import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
+
+export class Book {
+  constructor(public title: string,
+              public description: string,
+              public rating: number = 0) { }
+}
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
-  directives: [MdList, MdToolbar, MdButton, MdSlideToggle]
+  directives: [MD_LIST_DIRECTIVES, MD_TOOLBAR_DIRECTIVES]
 })
-export class AppComponent {
-  title = 'DWX app works!';
+export class AppComponent implements OnInit {
+  title: string = 'DWX app works!';
+  books: Book[];
+
+    constructor(private http: Http) { }
+
+    ngOnInit() {
+      this.http
+        .get('http://book-monkey2-api.angular2buch.de/books')
+        .subscribe(response => {
+          this.books = response.json();
+        });
+    }
 }
